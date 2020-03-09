675eb61bafaf9eda43da6075d6c33059cfafbd0c
BAlleyneUWO <balleyn@uwo.ca>
2020-03-09 19:45:43 -0400

    Overwriting default endpoint name

--- spring-data-rest-crud/src/main/java/com/bAlleyne/springboot/crudAppGH/dao/EmployeeRepository.java
+++ spring-data-rest-crud/src/main/java/com/bAlleyne/springboot/crudAppGH/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.bAlleyne.springboot.crudAppGH.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.bAlleyne.springboot.crudAppGH.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
