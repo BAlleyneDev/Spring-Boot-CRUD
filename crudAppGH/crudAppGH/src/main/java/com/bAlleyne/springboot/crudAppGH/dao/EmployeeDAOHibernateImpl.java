e6e3b59780251a86145340e0474f27f2219651cf
BAlleyneUWO <balleyn@uwo.ca>
2020-03-09 15:59:43 -0400

    CRUD features added in the DAO

--- crudAppGH/crudAppGH/src/main/java/com/bAlleyne/springboot/crudAppGH/dao/EmployeeDAOHibernateImpl.java
+++ crudAppGH/crudAppGH/src/main/java/com/bAlleyne/springboot/crudAppGH/dao/EmployeeDAOHibernateImpl.java
@@ -23,7 +23,6 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO{
 	}
 	
 	@Override
-	@Transactional
 	public List<Employee> findAll() {
 		//Native Hibernate API
 		// get the hibernate session
@@ -38,6 +37,43 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO{
 		
 		//return results
 		return employees;
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		//get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		//get the employee
+		Employee theEmployee = 
+				currentSession.get(Employee.class, theId);
+		
+		return theEmployee; 
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		
+		//get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		//save employee
+		currentSession.saveOrUpdate(theEmployee);
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		//get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		//delete object with primary key
+		Query theQuery =
+				currentSession.createQuery(
+						"delete from Employee where id=:emloyeeId");
+		theQuery.setParameter("employeeId", theId);
+		
+		theQuery.executeUpdate();
+		
 	}
 
 }
