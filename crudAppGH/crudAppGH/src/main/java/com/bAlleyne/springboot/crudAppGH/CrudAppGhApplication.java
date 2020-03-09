15b89aff9092f3c7dd5195248eae2df8cee16bf6
BAlleyneUWO <balleyn@uwo.ca>
2020-03-09 14:05:40 -0400

    Spring Boot Project Initialization

---
+++ crudAppGH/crudAppGH/src/main/java/com/bAlleyne/springboot/crudAppGH/CrudAppGhApplication.java
@@ -0,0 +1,13 @@
+package com.bAlleyne.springboot.crudAppGH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppGhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAppGhApplication.class, args);
+	}
+
+}
